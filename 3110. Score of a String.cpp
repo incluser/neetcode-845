974592fc8a9815944bafe7b704b50f56519e2969
Eldos Kurban <onetouchX9@gmail.com>
2025-11-20 17:24:29 +0500

    3110. Score of a String

---
+++ 3110. Score of a String.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <cmath>
+class Solution
+{
+public:
+      /* method sums up the ascii difference of adjacent chars */
+      int scoreOfString(std::string s)
+      {
+            int ans = 0;
+            for (int i = 1; i < s.length(); i++)
+            {
+                  /* important, we take absolute difference */
+                  ans += abs(s[i] - s[i - 1]);
+            }
+            return ans;
+      }
+};
